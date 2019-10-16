81f5e090ac53b9dd050a30cf201f3faf73ad5387
Anthony David Cobb <anthonydcobb@gmail.com>
2019-10-16 20:09:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // add solution here
+var musician = ["lennon", "mccartney", "ringo", "the other one"];
+var instrument = ["guitar", "also guitar", "drums", "this guy did whatever"]
+
+
+
+var theBeatlesPlay = (musicianArr, instrumentArr) => {
+    for (let i = 0; i >= musicianArr - 1; i++) {
+      console.log(`${musicianArr[i]} plays ${instrumentArr[i]}`)
+}
+
+
+
+theBeatlesPlay(musician, instrument);
