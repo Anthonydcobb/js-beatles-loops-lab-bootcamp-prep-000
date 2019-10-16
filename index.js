4f29dcbdeb98adb1dcd95058e1d0928c05d72863
Anthony David Cobb <anthonydcobb@gmail.com>
2019-10-16 20:13:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-var musician = ["lennon", "mccartney", "ringo", "the other one"];
-var instrument = ["guitar", "also guitar", "drums", "something"];
+var musician = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+var instrument = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 
 
 
 var theBeatlesPlay = (musicianArr, instrumentArr) => {
     for (var i = 0; i <= musicianArr.length - 1; i++) {
-      var beatlesArray = []
+      var beatlesArray = [];
       beatlesArray.push(`${musicianArr[i]} plays ${instrumentArr[i]}`)
     }
     return beatlesArray
